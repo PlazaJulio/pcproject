402c94918f8b580666e117a26be6c8dad44a9fe5
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-20 18:18:43 -0300

    Criado migration e model da tabela Acusacao e corrigido detalhes de Genero e GrauDeEscolaridade

--- API_PC/app/Models/Acusacao.php
+++ API_PC/app/Models/Acusacao.php
@@ -4,10 +4,10 @@ namespace App\Models;
  
 use Illuminate\Database\Eloquent\Model;
  
-class Genero extends Model
+class Acusacao extends Model
 {
-    protected $table = 'genero';
-    protected $fillable = ['genero'];
+    protected $table = 'acusacao';
+    protected $fillable = ['tipo'];
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';
 }
--- API_PC/app/Models/GrauDeEscolaridade.php
+++ API_PC/app/Models/GrauDeEscolaridade.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
  
 class GrauDeEscolaridade extends Model
 {
-    protected $table = 'estado_civil';
+    protected $table = 'grau_de_escolaridade';
     protected $fillable = ['grau'];
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';

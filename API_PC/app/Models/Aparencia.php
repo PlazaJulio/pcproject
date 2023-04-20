4f0eaabe6e59445b3ebc70eb5cad57af4e8d528c
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-20 18:34:26 -0300

    Refatorado migration e model de aparecia

--- API_PC/app/Models/Aparencia.php
+++ API_PC/app/Models/Aparencia.php
@@ -7,10 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Aparencia extends Model
 {
     protected $table = 'aparencia';
-    protected $fillable = ['altura', 'etnia', 'cor', 
-    'cor_do_cabelo', 'tipo_de_cabelo', 'cor_dos_olhos',
-    'foto_de_perfil_esquerdo', 'foto_de_perfil_direito',
-    'foto_de_frente'];
+    protected $fillable = ['altura', 'etnia', 'cor_id', 
+    'cor_do_cabelo_id', 'tipo_de_cabelo_id', 'cor_dos_olhos_id',
+    'porte_fisico_id','foto_de_perfil_esquerdo', 
+    'foto_de_perfil_direito', 'foto_de_frente'];
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';
 }

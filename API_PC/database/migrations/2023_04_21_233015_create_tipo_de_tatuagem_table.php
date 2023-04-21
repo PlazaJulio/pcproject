2603d9e2f2eb59d3a43faebbc91ca87722445ea1
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-21 20:40:20 -0300

    Criação da migration e model da tabela de TipoDeTatuagem

---
+++ API_PC/database/migrations/2023_04_21_233015_create_tipo_de_tatuagem_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tipo_de_tatuagem', function (Blueprint $table) {
+            $table->id();
+            $table->string("tipo");
+            $table->timestamp('criado_em')->useCurrent();
+            $table->timestamp('alterado_em')->useCurrentOnUpdate()->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tipo_de_tatuagem');
+    }
+};

6704290cdd9b612de5fd51f8b81c2cfaeb66eb41
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-24 17:35:47 -0300

    Criado model do tabela Marca

---
+++ API_PC/app/Models/Marca.php
@@ -0,0 +1,13 @@
+<?php
+ 
+namespace App\Models;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class PorteFisico extends Model
+{
+    protected $table = 'marca';
+    protected $fillable = ['cicatriz_ou_tatuagem', 'descricao', 'parte_do_corpo', 'foto', 'tipo', 'aparencia_id'];
+    const CREATED_AT = 'criado_em';
+    const UPDATED_AT = 'alterado_em';
+}

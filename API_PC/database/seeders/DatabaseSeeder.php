5176d96ac9b9ab55b51ded28b680454dcb81ebc5
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-24 18:36:30 -0300

    Criado a seeder de tipos de tatuagem

--- API_PC/database/seeders/DatabaseSeeder.php
+++ API_PC/database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call([CorSeeder::class]);
+        $this->call([CorSeeder::class, TipoTatuagem::class]);
     }
 }

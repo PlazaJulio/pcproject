fb2ae1285e4eb49d9db136fcf9e4057747fc5562
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-24 17:36:14 -0300

    Criado da migration da  tabela Marca

---
+++ API_PC/database/migrations/2023_04_24_202936_create_marca_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('marca', function (Blueprint $table) {
+            $table->id();
+            $table->string('cicatriz_ou_tatuagem');
+            $table->string('descricao');
+            $table->string('parte_do_corpo');
+            $table->string('foto');
+            $table->string('tipo');
+            
+            $table->unsignedBigInteger('aparencia_id');
+            $table->foreign('aparencia_id')->references('id')->on('aparencia');
+
+            $table->timestamp('criado_em')->useCurrent();
+            $table->timestamp('alterado_em')->useCurrentOnUpdate()->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('marca');
+    }
+};

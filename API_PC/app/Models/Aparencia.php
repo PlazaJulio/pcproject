f32808bc2d2796ceed579f6d43cb78374fe734d6
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-24 18:02:47 -0300

    Ajuste na organização do codigo da model de aparencia, nenhuma modificação

--- API_PC/app/Models/Aparencia.php
+++ API_PC/app/Models/Aparencia.php
@@ -7,10 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Aparencia extends Model
 {
     protected $table = 'aparencia';
-    protected $fillable = ['altura', 'etnia', 'cor_id', 
-    'cor_do_cabelo_id', 'tipo_de_cabelo_id', 'cor_dos_olhos_id',
-    'porte_fisico_id','foto_de_perfil_esquerdo', 
-    'foto_de_perfil_direito', 'foto_de_frente'];
+    protected $fillable = ['altura', 
+                           'etnia', 
+                           'cor_id', 
+                           'cor_do_cabelo_id', 
+                           'tipo_de_cabelo_id', 
+                           'cor_dos_olhos_id',
+                           'porte_fisico_id',
+                           'foto_de_perfil_esquerdo', 
+                           'foto_de_perfil_direito', 
+                           'foto_de_frente'];
+                           
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';
 }

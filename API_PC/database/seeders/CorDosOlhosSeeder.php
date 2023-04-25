9bceca6bdda16f2814a572a70b5ab0e5380cf221
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-25 17:52:24 -0300

    Criação da Seeder de CorDosOlhos

---
+++ API_PC/database/seeders/CorDosOlhosSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use app\Models\CorDosOlhos;
+
+class CorDosOlhosSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $corUm = new CorDosOlhos;
+        $corUm->cor = "Castanho";
+        $corUm->save();
+
+        $corDois = new CorDosOlhos;
+        $corDois->cor = "Azul";
+        $corDois->save();
+
+        $corTres = new CorDosOlhos;
+        $corTres->cor = "Cinza ";
+        $corTres->save();
+
+        $corQuatro = new CorDosOlhos;
+        $corQuatro->cor = "Verde ";
+        $corQuatro->save();
+
+        $corCinco = new CorDosOlhos;
+        $corCinco->cor = "Avelã";
+        $corCinco->save();
+
+        $corSeis = new CorDosOlhos;
+        $corSeis->cor = "Ambar";
+        $corSeis->save();
+    }
+}

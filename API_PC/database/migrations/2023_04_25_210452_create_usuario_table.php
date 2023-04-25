23071dd079cbabf9b4dadaaef1ce263a67f19ae5
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-04-25 18:07:06 -0300

    Criado migration para criar a tabela usuario

---
+++ API_PC/database/migrations/2023_04_25_210452_create_usuario_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('usuario');
+            $table->string('senha');
+            $table->string('nome');
+            $table->timestamp('criado_em')->useCurrent();
+            $table->timestamp('alterado_em')->useCurrentOnUpdate()->nullable(true);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('usuario');
+    }
+};

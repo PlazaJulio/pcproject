2b7d57340d300b6ce0245789171c5ebce80d2756
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-04-28 17:48:06 -0300

    Adicionado o arquivo RodeUmaVezSeeder, para que possamos facilitar o semeamento de nosso banco, usando o php artisan db:seed RodeUmaVezSeeder

---
+++ API_PC/database/seeders/RodeUmaVezSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Database\Seeders\AcusacaoSeeder;
+use Database\Seeders\CorDeCabeloSeeder;
+use Database\Seeders\CorDosOlhosSeeder;
+use Database\Seeders\CorSeeder;
+use Database\Seeders\GeneroSeeder;
+use Database\Seeders\PorteFisicoSeeder;
+use Database\Seeders\TipoDeCabeloSeeder;
+use Database\Seeders\TipoDeTatuagemSeeder;
+
+class RodeUmaVezSeeder extends Seeder
+{
+    public function run()
+    {
+        $this->call(
+            [
+                AcusacaoSeeder::class, 
+                CorDeCabeloSeeder::class, 
+                CorDosOlhosSeeder::class,
+                CorSeeder::class,
+                GeneroSeeder::class,
+                PorteFisicoSeeder::class,
+                TipoDeCabeloSeeder::class,
+                TipoDeTatuagemSeeder::class
+            ]
+        );
+    }
+}

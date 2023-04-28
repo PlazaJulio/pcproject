e3c959fc19fd4760311f805aea02ce5f07d230f5
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-04-28 18:46:29 -0300

    Teste de criação de controller e rota (Aparencia) !Incompleto!

---
+++ API_PC/app/Http/Controllers/AparenciaController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Models\Aparencia;
+use Exception;
+
+class AparenciaController extends Controller
+{
+    
+    public function __construct()
+    {
+        //
+    }
+    public function mostrarPorId($id)
+    {
+        return Aparencia::findOrFail($id);
+    }
+
+    public function mostrarTodos()
+    {
+        return Aparencia::all();
+    }
+
+    public function inserir(Request $request)
+    {   
+        try{
+            return Aparencia::create([
+                "altura" => $request->altura,
+                "etnia" => $request->etnia,
+                "cor_id" => $request->cor_id,
+                "cor_do_cabelo_id" => $request->cor_do_cabelo_id,
+                "tipo_de_cabelo_id" => $request->tipo_de_cabelo_id,
+                "cor_dos_olhos_id" => $request->cor_dos_olhos_id,
+                "porte_fisico_id" => $request->porte_fisico_id,
+                "foto_de_perfil_esquerdo" => $request->foto_de_perfil_esquerdo,
+                "foto_de_perfil_direito" => $request->foto_de_perfil_direito,
+                "foto_de_frente" => $request->foto_de_frente
+            ]);
+        }catch(Exception){
+            return response("Requisição feita de maneira incorreta", 400);
+        }
+    }
+}
--- API_PC/routes/web.php
+++ API_PC/routes/web.php
@@ -1,18 +1,12 @@
 <?php
 
 /** @var \Laravel\Lumen\Routing\Router $router */
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
+use Illuminate\Http\Request;
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+
+$router->get("/aparencia/{id}", "AparenciaController@mostrarPorId");
+$router->get("/aparencia", "AparenciaController@mostrarTodos");
+$router->post("/aparencia/inserir", "AparenciaController@inserir");

ab0bb6eeaa6c2f27c84e64347db4821ccd7906db
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-29 15:58:29 -0300

    Efetuado a criação do Marcafactory e MarcaSeeder

---
+++ API_PC/database/factories/MarcaFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Marca;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MarcaFactory extends Factory
+{
+    
+    protected $model = Marca::class;
+
+    public function definition()
+    {
+
+        $lista = ['c','t'];
+        $cOuT = $this->faker->randomElement($lista);
+
+        return [
+            'cicatriz_ou_tatuagem' => $cOuT,
+            'descricao' => $this->faker->sentence,
+            'parte_do_corpo' => $this->faker->word,
+            'foto' => $this->faker->sentence,
+            'tipo_de_tatuagem_id' => ($cOuT == 't')? random_int(1, 5): null,
+            'aparencia_id' => random_int(1, 10),
+        ];
+    }
+}

c966a2f65e84de9db74092e25f0db0de3c8766cd
Igor Sousa <igorrafaelsb248@gmail.com>
2023-04-29 14:44:57 -0300

    Apagado linhas não usadas no CorController

--- API_PC/routes/web.php
+++ API_PC/routes/web.php
@@ -13,10 +13,8 @@ $router->post("/aparencia/inserir", "AparenciaController@inserir");
 $router->delete("/aparencia/{id}", "AparenciaController@deletar");
 $router->patch("/aparencia/{id}", "AparenciaController@alterar");
 
-
 $router->get("/cor/{id}", "CorController@mostrarPorId");
 $router->get("/cor", "CorController@mostrarTodos");
-
 
 $router->get("/cor-do-cabelo/{id}", "CorDoCabeloController@mostrarPorId");
 $router->get("/cor-do-cabelo", "CorDoCabeloController@mostrarTodos");

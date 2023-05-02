4af494329b4bf932026e112b8ea6884123d5b4ac
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-02 18:17:53 -0300

    Criado Seeder de anteccedente usado sua factory e adicinado a lista de seeder a rodar automaticamente

--- API_PC/database/seeders/DatabaseSeeder.php
+++ API_PC/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
                 EnderecoSeeder::class,
                 MarcaSeeder::class,
                 CriminosoSeeder::class,
+                AntecedenteSeeder::class,
             ]
         );
     }

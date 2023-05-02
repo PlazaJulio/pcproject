60d88d366d0fc956651851f100cf209aa5c2338f
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-02 18:17:25 -0300

    Criado Factory da tabela antecedente e adicionado a sua model

--- API_PC/app/Models/Antecedente.php
+++ API_PC/app/Models/Antecedente.php
@@ -3,9 +3,17 @@
 namespace App\Models;
  
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Database\Factories\AntecedenteFactory;
  
 class Antecedente extends Model
 {
+    
+    protected static function factory(): Factory
+    {
+        return AntecedenteFactory::new();
+    }
+
     protected $table = 'antecedente';
     protected $fillable = ['local','data','hora','descricao','acusacao_id','criminoso_id'];
     const CREATED_AT = 'criado_em';
---
+++ API_PC/database/factories/AntecedenteFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Antecedente;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AntecedenteFactory extends Factory
+{
+    
+    protected $model = Antecedente::class;
+
+    public function definition()
+    {
+        
+        $listaData = ["2000-05-22", "2005-04-21", "2001-03-04", "2003-12-12"];
+        $listaHora = ["18:50:21", "15:20:10", "21:00:12", "12:20:12"];
+
+        return [
+            'local' => $this->faker->address,
+            'data' => $this->faker->randomElement($listaData),
+            'hora' => $this->faker->randomElement($listaHora),
+            'descricao' => $this->faker->sentence,
+            'acusacao_id' => random_int(1, 5),
+            'criminoso_id' => random_int(1, 10),
+        ];
+    }
+}

38087dbda117d813f4e111a5e7849df3e947a768
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-05-03 18:32:55 -0300

    Adicionado o controller para a tabela de tipo_de_tatuagem

---
+++ API_PC/app/Http/Controllers/TipoDeTatuagemController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\TipoDeTatuagem;
+use Exception;
+
+class TipoDeTatuagemController extends Controller
+{
+    public function mostrarPorId($id)
+    {
+        return TipoDeTatuagem::findOrFail($id);
+    }
+
+    public function mostrarTodos()
+    {
+        return TipoDeTatuagem::all();
+    }
+
+    public function inserir(Request $request)
+    {   
+        try{
+            return TipoDeTatuagem::create([
+                "tipo" => $request->tipo,
+            ]);
+        }catch(Exception){
+            return response("Requisição feita de maneira incorreta", 400);
+        }
+    }
+
+    public function deletar($id)
+    {
+        $dadoExcluido = TipoDeTatuagem::findOrFail($id);
+        $dadoExcluido->delete();
+        return $dadoExcluido;
+    }
+
+    public function alterar($id, Request $request)
+    {
+        $dadoASerAlterado = TipoDeTatuagem::findOrFail($id);
+        foreach ($request->except('_token') as $chave => $valor){
+            $dadoASerAlterado->update([$chave => $valor]);
+        }
+        return $dadoASerAlterado;
+    }
+    
+}

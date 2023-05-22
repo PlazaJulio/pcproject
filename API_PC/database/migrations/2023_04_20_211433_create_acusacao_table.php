f9822512350a9dea61f4f34e984967ca3416683f
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-05-22 17:46:01 -0300

    Adicionado o campo usuario_id nas migrations de: antecedente, aparencia e acusacao / alterado a data da migration de usuario para 2023_04_02

--- API_PC/database/migrations/2023_04_20_211433_create_acusacao_table.php
+++ API_PC/database/migrations/2023_04_20_211433_create_acusacao_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string("tipo");
             $table->timestamp('criado_em')->useCurrent();
             $table->timestamp('alterado_em')->useCurrentOnUpdate()->nullable(true);
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuario');
         });
     }
 

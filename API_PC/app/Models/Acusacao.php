aa767a99798b91f7a16da95c19946d68c2a31a6a
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-05-22 17:55:18 -0300

    Alterado as models das tabelas antecedente, aparencia e acusacao para possuir o campo usuario_id

--- API_PC/app/Models/Acusacao.php
+++ API_PC/app/Models/Acusacao.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Acusacao extends Model
 {
     protected $table = 'acusacao';
-    protected $fillable = ['tipo'];
+    protected $fillable = ['tipo', 'usuario_id'];
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';
 }

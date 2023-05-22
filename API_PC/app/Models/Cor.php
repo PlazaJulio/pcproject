0d11526593b4c346c41f8c63a20dac64160a0379
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-05-22 18:20:53 -0300

    Adicionado o campo usuario_id nas models de: estado_civil, genero e cor

--- API_PC/app/Models/Cor.php
+++ API_PC/app/Models/Cor.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Cor extends Model
 {
     protected $table = 'cor';
-    protected $fillable = ['cor'];
+    protected $fillable = ['cor', 'usuario_id'];
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';
 }

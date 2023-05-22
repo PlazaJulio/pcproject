ff8aaa8332afdd31e6ec5a3652249a1958fef460
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-22 18:36:21 -0300

    Efetuado a modificação na Seeder de grau de escolaridade

--- API_PC/database/seeders/GrauDeEscolaridadeSeeder.php
+++ API_PC/database/seeders/GrauDeEscolaridadeSeeder.php
@@ -15,26 +15,32 @@ class GrauDeEscolaridadeSeeder extends Seeder
     {
         $escolaridadeUm = new GrauDeEscolaridade;
         $escolaridadeUm->grau = "fundamental incompleto";
+        $escolaridadeUm->usuario_id = 1;
         $escolaridadeUm->save();
 
         $escolaridadeDois = new GrauDeEscolaridade;
         $escolaridadeDois->grau = "fundamental completo";
+        $escolaridadeDois->usuario_id = 1;
         $escolaridadeDois->save();
 
         $escolaridadeTres = new GrauDeEscolaridade;
         $escolaridadeTres->grau = "médio incompleto";
+        $escolaridadeTres->usuario_id = 1;
         $escolaridadeTres->save();
 
         $escolaridadeQuatro = new GrauDeEscolaridade;
         $escolaridadeQuatro->grau = "ensino médio completo";
+        $escolaridadeQuatro->usuario_id = 1;
         $escolaridadeQuatro->save();
 
         $escolaridadeCinco = new GrauDeEscolaridade;
         $escolaridadeCinco->grau = "superior incompleto";
+        $escolaridadeCinco->usuario_id = 1;
         $escolaridadeCinco->save();
 
         $escolaridadeSeis = new GrauDeEscolaridade;
         $escolaridadeSeis->grau = "superior completo";
+        $escolaridadeSeis->usuario_id = 1;
         $escolaridadeSeis->save();
     }
 }

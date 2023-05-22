9ff269d9d3a99c9f246d08f7bffbe6a3f848be34
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-22 18:20:43 -0300

    Efetuado a modificação da Migration de grau de escolaridade

--- API_PC/database/migrations/2023_04_20_210559_create_grau_de_escolaridade_table.php
+++ API_PC/database/migrations/2023_04_20_210559_create_grau_de_escolaridade_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('grau_de_escolaridade', function (Blueprint $table) {
             $table->id();
             $table->string("grau");
+
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+
             $table->timestamp('criado_em')->useCurrent();
             $table->timestamp('alterado_em')->useCurrentOnUpdate()->nullable(true);
         });

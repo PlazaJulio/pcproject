6d5e7d36d3dd27dadc18de8e137a6c8ec3cb1c79
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-23 17:29:35 -0300

    Efetuado a modificação da Migration de marca

--- API_PC/database/migrations/2023_04_24_202936_create_marca_table.php
+++ API_PC/database/migrations/2023_04_24_202936_create_marca_table.php
@@ -24,6 +24,9 @@ return new class extends Migration
             $table->unsignedBigInteger('aparencia_id');
             $table->foreign('aparencia_id')->references('id')->on('aparencia');
 
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+
             $table->timestamp('criado_em')->useCurrent();
             $table->timestamp('alterado_em')->useCurrentOnUpdate()->nullable(true);
         });

d20a46d10dbcd60197209d5821400fa310bba297
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-23 18:01:02 -0300

    Efetuado a modificação da Seeder de tipo_de_tatuagem

--- API_PC/database/seeders/TipoDeTatuagemSeeder.php
+++ API_PC/database/seeders/TipoDeTatuagemSeeder.php
@@ -12,22 +12,27 @@ class TipoDeTatuagemSeeder extends Seeder
     {
         $tatooUm = new TipoDeTatuagem;
         $tatooUm->tipo = 'Dragao';
+        $tatooUm->usuario_id = 1;
         $tatooUm->save();
 
         $tatooDois = new TipoDeTatuagem;
         $tatooDois->tipo = 'Pokemon';
+        $tatooDois->usuario_id = 1;
         $tatooDois->save();
 
         $tatooTres = new TipoDeTatuagem;
         $tatooTres->tipo = 'Faca';
+        $tatooTres->usuario_id = 1;
         $tatooTres->save();
 
         $tatooQuatro = new TipoDeTatuagem;
         $tatooQuatro->tipo = 'Arma ';
+        $tatooQuatro->usuario_id = 1;
         $tatooQuatro->save();
 
         $tatooCinco = new TipoDeTatuagem;
         $tatooCinco->tipo = 'Palhaco ';
+        $tatooCinco->usuario_id = 1;
         $tatooCinco->save();
     }
 }

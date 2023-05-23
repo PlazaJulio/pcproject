780bb96359f9c73a7b24beee0d99427ec5a60f81
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-23 18:19:37 -0300

    Efetuado a modificação (Add coluna excluido) da Controller de aparencia

--- API_PC/app/Http/Controllers/AparenciaController.php
+++ API_PC/app/Http/Controllers/AparenciaController.php
@@ -37,7 +37,8 @@ class AparenciaController extends Controller
                 "foto_de_perfil_esquerdo" => $request->foto_de_perfil_esquerdo,
                 "foto_de_perfil_direito" => $request->foto_de_perfil_direito,
                 "foto_de_frente" => $request->foto_de_frente,
-                "usuario_id" => $request->usuario_id
+                "usuario_id" => $request->usuario_id,
+                "excluido" => false
             ]);
         }catch(Exception){
             return response("Requisição feita de maneira incorreta", 400);

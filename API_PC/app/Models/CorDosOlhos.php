9e383d302d4d0e46036c939ab7024887dfe4d07b
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-23 18:32:15 -0300

    Efetuado a modificação (Add coluna excluido) da Model de cor_dos_olhos

--- API_PC/app/Models/CorDosOlhos.php
+++ API_PC/app/Models/CorDosOlhos.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class CorDosOlhos extends Model
 {
     protected $table = 'cor_dos_olhos';
-    protected $fillable = ['cor', 'usuario_id'];
+    protected $fillable = ['cor', 'usuario_id', 'excluido'];
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';
 }

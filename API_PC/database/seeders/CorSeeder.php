d569636ed310fb69eb3fb40ed13493b48b365ced
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-05-23 17:34:32 -0300

    Alterado os seeders das tabelas: cor, estado_civil e genero para inserir o valor de usuario_id

--- API_PC/database/seeders/CorSeeder.php
+++ API_PC/database/seeders/CorSeeder.php
@@ -11,22 +11,27 @@ class CorSeeder extends Seeder
     {
         $corAmarela = new Cor;
         $corAmarela->cor = "amarelo";
+        $corAmarela->usuario_id = 1;
         $corAmarela->save();
 
         $corBranco = new Cor;
         $corBranco->cor = "braco";
+        $corBranco->usuario_id = 1;
         $corBranco->save();
 
         $corPardo = new Cor;
         $corPardo->cor = "pardo";
+        $corPardo->usuario_id = 1;
         $corPardo->save();
 
         $corIndigena = new Cor;
         $corIndigena->cor = "indigena";
+        $corIndigena->usuario_id = 1;
         $corIndigena->save();
 
         $corPreto = new Cor;
         $corPreto->cor = "preto";
+        $corPreto->usuario_id = 1;
         $corPreto->save();
     }
 }

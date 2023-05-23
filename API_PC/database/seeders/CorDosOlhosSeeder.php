bf0c7d61e12254576cae18eb43cd21522029bf60
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-23 18:46:28 -0300

    Efetuado a modificação (Add coluna excluido) da Seeder de cor_dos_olhos

--- API_PC/database/seeders/CorDosOlhosSeeder.php
+++ API_PC/database/seeders/CorDosOlhosSeeder.php
@@ -13,31 +13,37 @@ class CorDosOlhosSeeder extends Seeder
         $corUm = new CorDosOlhos;
         $corUm->cor = "Castanho";
         $corUm->usuario_id = 1;
+        $corUm->excluido = false;
         $corUm->save();
 
         $corDois = new CorDosOlhos;
         $corDois->cor = "Azul";
         $corDois->usuario_id = 1;
+        $corDois->excluido = false;
         $corDois->save();
 
         $corTres = new CorDosOlhos;
         $corTres->cor = "Cinza ";
         $corTres->usuario_id = 1;
+        $corTres->excluido = false;
         $corTres->save();
 
         $corQuatro = new CorDosOlhos;
         $corQuatro->cor = "Verde ";
         $corQuatro->usuario_id = 1;
+        $corQuatro->excluido = false;
         $corQuatro->save();
 
         $corCinco = new CorDosOlhos;
         $corCinco->cor = "Avelã";
         $corCinco->usuario_id = 1;
+        $corCinco->excluido = false;
         $corCinco->save();
 
         $corSeis = new CorDosOlhos;
         $corSeis->cor = "Ambar";
         $corSeis->usuario_id = 1;
+        $corSeis->excluido = false;
         $corSeis->save();
     }
 }

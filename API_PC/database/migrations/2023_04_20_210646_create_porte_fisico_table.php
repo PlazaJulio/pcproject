22a1097e92f582e8cfe808646ec5802e8f33a333
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-23 17:48:28 -0300

    Efetuado a modificação da Migration de porte_fisico

--- API_PC/database/migrations/2023_04_20_210646_create_porte_fisico_table.php
+++ API_PC/database/migrations/2023_04_20_210646_create_porte_fisico_table.php
@@ -14,6 +14,10 @@ return new class extends Migration
         Schema::create('porte_fisico', function (Blueprint $table) {
             $table->id();
             $table->string("porte");
+
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('usuario');
+            
             $table->timestamp('criado_em')->useCurrent();
             $table->timestamp('alterado_em')->useCurrentOnUpdate()->nullable(true);
         });

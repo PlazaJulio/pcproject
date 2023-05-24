604eac8101512a115376b7daaf1b84475c558561
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-24 14:35:43 -0300

    Efetuado modificação na organização do codigo

--- API_PC/app/Http/Controllers/CriminosoController.php
+++ API_PC/app/Http/Controllers/CriminosoController.php
@@ -47,8 +47,8 @@ class CriminosoController extends Controller
                 "genero_id" => $request->genero_id,
                 "endereco_id" => $request->endereco_id,
                 "aparencia_id" => $request->aparencia_id,
-                "excluido" => false,
-                "usuario_id" => $request->usuario_id
+                "usuario_id" => $request->usuario_id,
+                "excluido" => false
                 
             ]);
         }catch(Exception){

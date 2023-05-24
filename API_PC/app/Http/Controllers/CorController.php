9728c71d4f5d3cb5e0264ab21bdf81a756da2216
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-24 17:57:29 -0300

    Efetuado a criação das funções de Inserir, Delete e Alterar no Controller de Cor

--- API_PC/app/Http/Controllers/CorController.php
+++ API_PC/app/Http/Controllers/CorController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+namespace App\Http\Controllers;
+use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use Exception;
 use App\Models\Cor;
 
 class CorController extends Controller
@@ -19,5 +23,34 @@ class CorController extends Controller
     public function mostrarTodos()
     {
         return Cor::all();
+    }
+
+    public function inserir(Request $request)
+    {   
+        try{
+            return Cor::create([
+                "cor" => $request->cor,
+                "usuario_id" => auth()->user()->id,
+                "excluido" => false
+            ]);
+        }catch(Exception){
+            return response("Requisição feita de maneira incorreta", 400);
+        }
+    }
+
+    public function deletar($id)
+    {
+        $dadoExcluido = Cor::findOrFail($id);
+        $dadoExcluido->delete();
+        return $dadoExcluido;
+    }
+
+    public function alterar($id, Request $request)
+    {
+        $dadoASerAlterado = Cor::findOrFail($id);
+        foreach ($request->except('_token') as $chave => $valor){
+            $dadoASerAlterado->update([$chave => $valor]);
+        }
+        return $dadoASerAlterado;
     }
 }

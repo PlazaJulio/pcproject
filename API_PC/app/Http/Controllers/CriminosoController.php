7fc2d911233841e0283c67ddd0202da6a2bf102a
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-25 18:10:52 -0300

    Efetuado alteração na Controller de criminoso para pegar o id do usuario atual

--- API_PC/app/Http/Controllers/CriminosoController.php
+++ API_PC/app/Http/Controllers/CriminosoController.php
@@ -47,7 +47,7 @@ class CriminosoController extends Controller
                 "genero_id" => $request->genero_id,
                 "endereco_id" => $request->endereco_id,
                 "aparencia_id" => $request->aparencia_id,
-                "usuario_id" => $request->usuario_id,
+                "usuario_id" => auth()->user()->id,
                 "excluido" => false
                 
             ]);

02dffec410ac52d2fff427c769dbfa7bd66700ee
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-25 18:15:06 -0300

    Efetuado alteração na Controller de marca para pegar o id do usuario atual

--- API_PC/app/Http/Controllers/MarcaController.php
+++ API_PC/app/Http/Controllers/MarcaController.php
@@ -27,7 +27,7 @@ class MarcaController extends Controller
                 "foto" => $request->foto,
                 "tipo_de_tatuagem_id" => $request->tipo_de_tatuagem_id,
                 "aparencia_id" => $request->aparencia_id,
-                "usuario_id" => $request->usuario_id,
+                "usuario_id" => auth()->user()->id,
                 "excluido" => false
                 
             ]);

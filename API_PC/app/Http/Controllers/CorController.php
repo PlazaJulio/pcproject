3e68fbd582151a60d628c33463b7645baa786ea7
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-29 17:20:25 -0300

    Alterado a logica do metodo Alterar para pegar o id do usuario logado na Controller de Cor

--- API_PC/app/Http/Controllers/CorController.php
+++ API_PC/app/Http/Controllers/CorController.php
@@ -52,8 +52,13 @@ class CorController extends Controller
     {
         $dadoASerAlterado = Cor::findOrFail($id);
         foreach ($request->except('_token') as $chave => $valor){
+           if($chave == "excluido" || $chave == "usuairo_id")
+           {
+                continue;
+           }
             $dadoASerAlterado->update([$chave => $valor]);
         }
+        $dadoASerAlterado->update(["usuario_id" => auth()->user()->id]);
         return $dadoASerAlterado;
     }
 }

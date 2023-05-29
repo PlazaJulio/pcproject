6c0a211363857aa85e68581b2e53772e3b200927
Igor Sousa <igorrafaelsb248@gmail.com>
2023-05-29 17:20:52 -0300

    Alterado a logica do metodo Alterar para pegar o id do usuario logado na Controller de CorDoCabelo

--- API_PC/app/Http/Controllers/CorDoCabeloController.php
+++ API_PC/app/Http/Controllers/CorDoCabeloController.php
@@ -51,8 +51,13 @@ class CorDoCabeloController extends Controller
     {
         $dadoASerAlterado = CorDoCabelo::findOrFail($id);
         foreach ($request->except('_token') as $chave => $valor){
+           if($chave == "excluido" || $chave == "usuairo_id")
+           {
+                continue;
+           }
             $dadoASerAlterado->update([$chave => $valor]);
         }
+        $dadoASerAlterado->update(["usuario_id" => auth()->user()->id]);
         return $dadoASerAlterado;
     }
 }

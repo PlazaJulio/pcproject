6ab69225c549bac13f56d974817b70f1296b7833
Igor Sousa <igorrafaelsb248@gmail.com>
2023-06-06 17:25:17 -0300

    Efetuado ajuste na logica de deletar e alterar para tratar erros no Controller de Antecedente

--- API_PC/app/Http/Controllers/AntecedenteController.php
+++ API_PC/app/Http/Controllers/AntecedenteController.php
@@ -47,24 +47,33 @@ class AntecedenteController extends Controller
 
     public function deletar($id)
     {
-        $dadoExcluido = Antecedente::findOrFail($id);
-        $dadoExcluido->update(["excluido" => true]);
-        $dadoExcluido->update(["usuario_id" =>  auth()->user()->id]);
-        return $dadoExcluido;
+        
+        try{
+            $dadoExcluido = Antecedente::findOrFail($id);
+            $dadoExcluido->update(["excluido" => true]);
+            $dadoExcluido->update(["usuario_id" =>  auth()->user()->id]);
+            return $dadoExcluido;
+        }catch(Exception){
+            return response("", 404);
+        }
     }
 
     public function alterar($id, Request $request)
     {
-        $dadoASerAlterado = Antecedente::findOrFail($id);
-        foreach ($request->except('_token') as $chave => $valor){
-           if($chave == "excluido" || $chave == "usuario_id")
-           {
-                continue;
-           }
-            $dadoASerAlterado->update([$chave => $valor]);
+        try{
+            $dadoASerAlterado = Antecedente::findOrFail($id);
+            foreach ($request->except('_token') as $chave => $valor){
+                if($chave == "excluido" || $chave == "usuario_id")
+                {
+                    continue;
+                }
+                $dadoASerAlterado->update([$chave => $valor]);
+            }
+            $dadoASerAlterado->update(["usuario_id" => auth()->user()->id]);
+            return $dadoASerAlterado;
+        }catch(Exception){
+            return response("", 404);
         }
-        $dadoASerAlterado->update(["usuario_id" => auth()->user()->id]);
-        return $dadoASerAlterado;
     }
     
 }

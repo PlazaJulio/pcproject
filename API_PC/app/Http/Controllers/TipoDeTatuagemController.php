af7ffa0eefe03df1a7ac4bbb7d2cb8275e18286e
Igor Sousa <igorrafaelsb248@gmail.com>
2023-06-07 18:59:39 -0300

    Alterado logica de consulta do metodo de Deletar da Controller de TipoDeTatuagem

--- API_PC/app/Http/Controllers/TipoDeTatuagemController.php
+++ API_PC/app/Http/Controllers/TipoDeTatuagemController.php
@@ -36,7 +36,7 @@ class TipoDeTatuagemController extends Controller
     public function deletar($id)
     {
         try{
-            $dadoExcluido = TipoDeTatuagem::findOrFail($id);
+            $dadoExcluido = TipoDeTatuagem::where('excluido', false)->findOrFail($id);
             $dadoExcluido->update(["excluido" => true]);
             $dadoExcluido->update(["usuario_id" =>  auth()->user()->id]);
             return $dadoExcluido;

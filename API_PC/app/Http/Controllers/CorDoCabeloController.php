92fb05c3a1eec41558a2787b59dd92c7c8368d3b
Igor Sousa <igorrafaelsb248@gmail.com>
2023-06-07 17:20:07 -0300

    Efetuado ajuste na logica de deletar e alterar para tratar erros no Controller de CorDoCabelo

--- API_PC/app/Http/Controllers/CorDoCabeloController.php
+++ API_PC/app/Http/Controllers/CorDoCabeloController.php
@@ -42,23 +42,31 @@ class CorDoCabeloController extends Controller
 
     public function deletar($id)
     {
-        $dadoExcluido = CorDoCabelo::findOrFail($id);
-        $dadoExcluido->update(["excluido" => true]);
-        $dadoExcluido->update(["usuario_id" =>  auth()->user()->id]);
-        return $dadoExcluido;
+        try{
+            $dadoExcluido = CorDoCabelo::findOrFail($id);
+            $dadoExcluido->update(["excluido" => true]);
+            $dadoExcluido->update(["usuario_id" =>  auth()->user()->id]);
+            return $dadoExcluido;
+        }catch(Exception){
+            return response("", 404);
+        }
     }
 
     public function alterar($id, Request $request)
     {
-        $dadoASerAlterado = CorDoCabelo::findOrFail($id);
-        foreach ($request->except('_token') as $chave => $valor){
-           if($chave == "excluido" || $chave == "usuario_id")
-           {
-                continue;
-           }
-            $dadoASerAlterado->update([$chave => $valor]);
+        try{
+            $dadoASerAlterado = CorDoCabelo::findOrFail($id);
+            foreach ($request->except('_token') as $chave => $valor){
+                if($chave == "excluido" || $chave == "usuario_id")
+                {
+                    continue;
+                }
+                $dadoASerAlterado->update([$chave => $valor]);
+            }
+            $dadoASerAlterado->update(["usuario_id" => auth()->user()->id]);
+            return $dadoASerAlterado;
+        }catch(Exception){
+            return response("", 404);
         }
-        $dadoASerAlterado->update(["usuario_id" => auth()->user()->id]);
-        return $dadoASerAlterado;
     }
 }

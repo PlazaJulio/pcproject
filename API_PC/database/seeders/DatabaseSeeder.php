3ef3bc6e219ac3f519938b8583f7983d9f6c286d
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-06-22 17:42:26 -0300

    Deletado a tabela Aparencia e tudo que esta relacinado a ela

--- API_PC/database/seeders/DatabaseSeeder.php
+++ API_PC/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(
             [
-                AparenciaSeeder::class,
                 EnderecoSeeder::class,
                 MarcaSeeder::class,
                 CriminosoSeeder::class,

a41376fde27071978ce6de1a1ae810df6ddbe415
Igor Sousa <igorrafaelsb248@gmail.com>
2023-06-23 23:56:40 -0300

    Adicionado a relação de N x n na Model de Antecedente

--- API_PC/app/Models/Antecedente.php
+++ API_PC/app/Models/Antecedente.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Database\Factories\AntecedenteFactory;
+use App\Models\Criminoso;
  
 class Antecedente extends Model
 {
@@ -14,8 +15,13 @@ class Antecedente extends Model
         return AntecedenteFactory::new();
     }
 
+    public function criminosos()
+    {
+        return $this->belongsToMany(Criminoso::class, 'criminoso_antecedente', 'antecedente_id', 'criminoso_id');
+    }
+
     protected $table = 'antecedente';
-    protected $fillable = ['local','data','hora','descricao','acusacao_id','criminoso_id', 'usuario_id','excluido'];
+    protected $fillable = ['local','data','hora','descricao','acusacao_id', 'usuario_id','excluido'];
     const CREATED_AT = 'criado_em';
     const UPDATED_AT = 'alterado_em';
 }

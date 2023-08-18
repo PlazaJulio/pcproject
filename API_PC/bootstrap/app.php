beedf8fb68730586b700d21aaa3bf5b33ef14628
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-08-18 18:48:41 -0300

    Resolvido problema de cors

--- API_PC/bootstrap/app.php
+++ API_PC/bootstrap/app.php
@@ -97,6 +97,8 @@ $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 
+$app->middleware([App\Http\Middleware\CorsMiddleware::class]);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes

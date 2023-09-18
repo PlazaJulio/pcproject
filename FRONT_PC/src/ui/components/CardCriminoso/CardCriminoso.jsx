9d9408c7bfc688d47f53accb415da8c9f502324e
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-09-18 18:37:58 -0300

    Corrigido o bug de data de nascimento nos cards

--- FRONT_PC/src/ui/components/CardCriminoso/CardCriminoso.jsx
+++ FRONT_PC/src/ui/components/CardCriminoso/CardCriminoso.jsx
@@ -6,14 +6,20 @@ import calcularIdade from "../../../data/utils/calcularIdade";
 import base64EmImagem from '../../../data/utils/base64EmImagem';
 
 export default function CardCriminoso({ nomeCriminoso, imagem, dataNasc }) {
-    const dataN = new Date(dataNasc)
+    const dataString = dataNasc;
+    const partesData = dataString.split('-');
+    const ano = parseInt(partesData[0], 10);
+    const mes = parseInt(partesData[1], 10) - 1; 
+    const dia = parseInt(partesData[2], 10);
+    const data = new Date(ano, mes, dia);
+        
     return <div className="card m-2">
         <figure className="card-image">
             <img src={base64EmImagem(imagem)} />
         </figure>
         <div className="card-content">
             <p><b>{nomeCriminoso}</b></p>
-            <p><i>{dataN.toLocaleDateString("pt-BR")} - ({calcularIdade(dataN.getFullYear(), dataN.getMonth(), dataN.getDay())})</i></p>
+            <p><i>{data.toLocaleDateString("pt-BR")} - {calcularIdade(ano, mes, dia)}</i></p>
         </div>
         <div className="card-footer is-flex is-justify-content-center">
             <div className='mr-4 mt-4 mb-4'>

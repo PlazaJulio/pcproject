f6bbff4abfca50337c9c0b115b05a1a67b7a16f7
RenanKawamoto <renan_kawamoto@hotmail.com>
2023-09-22 18:31:43 -0300

    Finalizado lógica de vizualização basica do criminoso, mostrando suas marcas imagens etc

--- FRONT_PC/src/ui/components/CardCriminoso/CardCriminoso.jsx
+++ FRONT_PC/src/ui/components/CardCriminoso/CardCriminoso.jsx
@@ -26,6 +26,7 @@ export default function CardCriminoso({ id, nomeCriminoso, imagem, dataNasc, atu
     const [conteudoPopup, setConteudoPopup] = useState("");
     const { tokenReact } = useContext(TokenContext)
     const [objMarca, setObjMarca] = useState(null);
+    const [objTipoDeTatuagem, setObjTipoDeTatuagem] = useState(null);
     const [loading, setLoading] = useState(false);
 
     useEffect(() => {
@@ -35,9 +36,19 @@ export default function CardCriminoso({ id, nomeCriminoso, imagem, dataNasc, atu
                 try {
                     const responseMarca = await requestGet("/marca/criminoso/" + id, {}, tokenReact);
                     const responseDataMarca = responseMarca.data;
+                    responseDataMarca = await responseDataMarca.map(async (marca)=>{
+                        const pegarTipoDeTatuagem = async() => {
+                            if(marca.tipo_de_tatuagem_id != null){
+                                const tipo = await requestGet("/tipo-de-tatuagem/"+ marca.tipo_de_tatuagem_id, {}, tokenReact)
+                                return tipo.data.tipo                           
+                            }
+                            return null
+                        }
+                        marca.tipo_de_tatuagem = await pegarTipoDeTatuagem()
+                    })
+                    console.log(responseDataMarca)
                     if (responseDataMarca != []) {
                         setObjMarca(responseDataMarca);
-                        console.log(responseDataMarca);
                     }
                 } catch (error) {
                 }
@@ -48,6 +59,11 @@ export default function CardCriminoso({ id, nomeCriminoso, imagem, dataNasc, atu
             setLoading(false)
         }
     }, [modalViewEnable]);
+    async function pegarTipoDeTatuagem(id) {
+        const responseTipoDeTatuagem = await requestGet("/tipo-de-tatuagem/" + marca.tipo_de_tatuagem_id, {}, tokenReact);
+        const responseDataTipoDeTatuagem = responseTipoDeTatuagem.data;
+        setObjTipoDeTatuagem(responseDataTipoDeTatuagem);
+    }
     return <>
         {
             popupSucesso &&
@@ -99,21 +115,25 @@ export default function CardCriminoso({ id, nomeCriminoso, imagem, dataNasc, atu
                 titulo="Dados do criminoso:"
                 conteudo={
                     <ul>
+                        <b><span className='is-size-4'>Dados pessoais:</span></b>
                         <li><b>Nome:</b> {criminosoObj.nome}</li>
                         <li><b>Alcunha:</b> {criminosoObj.alcunha}</li>
-                        <li><b>Telefone:</b> {criminosoObj.telefone}</li>
+                        <li><b>Genero:</b> {criminosoObj.genero}</li>
+                        <li><b>Data de nascimento:</b> {data.toLocaleDateString("pt-BR")}</li>
                         <li><b>Nome do pai:</b> {criminosoObj.pai}</li>
                         <li><b>Nome da mãe:</b> {criminosoObj.mae}</li>
-                        <li><b>Obito:</b> {criminosoObj.obito ? "Verdade" : "Falso"}</li>
-                        <li><b>Foragido:</b> {criminosoObj.foragido ? "Verdade" : "Falso"}</li>
                         <li><b>RG:</b> {criminosoObj.rg}</li>
                         <li><b>CPF:</b> {criminosoObj.cpf}</li>
+                        <li><b>Telefone:</b> {criminosoObj.telefone}</li>
+                        <li><b>Obito:</b> {criminosoObj.obito ? "Verdade" : "Falso"}</li>
+                        <li><b>Foragido:</b> {criminosoObj.foragido ? "Verdade" : "Falso"}</li>
                         <li><b>Naturalidade:</b> {criminosoObj.naturalidade}</li>
                         <li><b>Nacionalidade:</b> {criminosoObj.nacionalidade}</li>
                         <li><b>Local de trabalho:</b> {criminosoObj.local_de_trabalho}</li>
                         <li><b>Profissao:</b> {criminosoObj.profissao}</li>
                         <li><b>Grau de Escolaridade:</b> {criminosoObj.grau_de_escolaridade}</li>
-                        <li><b>Genero:</b> {criminosoObj.genero}</li>
+                        <hr></hr>
+                        <b><span className='is-size-4'>Dados fisicos:</span></b>
                         <li><b>Altura:</b> {criminosoObj.altura}</li>
                         <li><b>Etnia:</b> {criminosoObj.etnia}</li>
                         <li><b>Porte Fisico:</b> {criminosoObj.porte_fisico}</li>
@@ -121,10 +141,13 @@ export default function CardCriminoso({ id, nomeCriminoso, imagem, dataNasc, atu
                         <li><b>Cor da pele:</b> {criminosoObj.cor_da_pele}</li>
                         <li><b>Cor do cabelo:</b> {criminosoObj.cor_do_cabelo}</li>
                         <li><b>Tipo de cabelo:</b> {criminosoObj.tipo_de_cabelo}</li>
+                        <hr></hr>
+                        <b><span className='is-size-4'>Fotos:</span></b>
                         <li><b>Foto de frente:</b> <figure className='image  is-128x128'><img src={base64EmImagem(criminosoObj.foto_frente)}></img></figure></li>
                         <li><b>Foto perfil esquerdo:</b> <figure className='image  is-128x128'><img src={base64EmImagem(criminosoObj.foto_perfil_esquerdo)}></img></figure></li>
                         <li><b>Foto perfil direito:</b> <figure className='image  is-128x128'><img src={base64EmImagem(criminosoObj.foto_perfil_direito)}></img></figure></li>
-                        <li><b>Data de nascimento:</b> {data.toLocaleDateString("pt-BR")}</li>
+                        <hr></hr>
+                        <b><span className='is-size-4'>Endereço:</span></b>
                         <li><b>CEP:</b> {criminosoObj.cep}</li>
                         <li><b>Rua:</b> {criminosoObj.rua}</li>
                         <li><b>Bairro:</b> {criminosoObj.bairro}</li>
@@ -132,8 +155,16 @@ export default function CardCriminoso({ id, nomeCriminoso, imagem, dataNasc, atu
                         <li><b>Complemento:</b> {criminosoObj.complemento}</li>
                         {
                             objMarca &&
-                            objMarca.map(() => {
-                                <p>oi</p>
+                            objMarca.map((tatuagem, index) => {
+                                return <>
+                                    <hr></hr>
+                                    <b><span className='is-size-4'>{index + 1}º - Marca</span></b>
+                                    <li><b>Cicatriz ou tatuagem:</b> {tatuagem.cicatriz_ou_tatuagem == 'c' ? "cicatriz" : "tatuagem"}</li>
+                                    <li><b>Tipo da tatuagem: </b> {tatuagem.tipo_de_tatuagem == null ? "Não é uma tatuagem" : tatuagem.tipo_de_tatuagem}</li>
+                                    <li><b>Descrição da marca:</b> {tatuagem.descricao}</li>
+                                    <li><b>Parte do corpo com a marca:</b> {tatuagem.parte_do_corpo}</li>
+                                    <li><b>Foto da marca:</b> <figure className='image  is-128x128'><img src={base64EmImagem(tatuagem.foto)}></img></figure></li>                                    
+                                </>
                             })
                         }
                     </ul>
